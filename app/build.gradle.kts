d4564561a985767647c0be31fe753c4d600e5e1d
im3an <imran.moujtahid@smail.th-koeln.de>
2026-01-14 16:53:18 +0100

    Gradle issues fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt.android)
-    alias(libs.plugins.kotlin.kapt)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
