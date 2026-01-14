1d8dce947cc798710f0e0577fe0e82ba63f0d356
im3an <imran.moujtahid@smail.th-koeln.de>
2026-01-14 16:50:06 +0100

    build.gradle.kts build error fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt.android)
-    alias(libs.plugins.kotlin.kapt)
+    id("org.jetbrains.kotlin.kapt") version "2.0.21"
 }
 
 android {
