d7e28fac8d508e083db0eb6fb2fdf672266d01cb
im3an <imran.moujtahid@smail.th-koeln.de>
2026-01-14 23:47:38 +0100

    Daschboard UI Material3

--- app/src/main/java/com/example/binbuddy/ui/MainActivity.java
+++ app/src/main/java/com/example/binbuddy/ui/MainActivity.java
@@ -11,12 +11,14 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import dagger.hilt.android.AndroidEntryPoint;
 
 import com.example.binbuddy.R;
 import com.example.binbuddy.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
